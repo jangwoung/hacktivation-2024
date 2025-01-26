573219413e218ff1faf1c1b8734437dd7b2eaeba
jangwoung <masao31536@gmail.com>
2025-01-27 00:51:15 +0900

    [add] Home frontend

--- packages/frontend/app/page.tsx
+++ packages/frontend/app/page.tsx
@@ -1,9 +1,73 @@
-import { ConnectButton } from "@rainbow-me/rainbowkit";
-
 export default function Home() {
   return (
-    <div className="flex w-full justify-center items-center p-20">
-      <ConnectButton />
+    <div className="flex flex-col items-center">
+      <div className="flex w-full justify-center items-center pt-12 pb-10">
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="検索ワードを入力してください"
+          className="min-w-[32vw] border-[#CECECE] border-b-2 text-center text-lg focus:outline-none focus:border-[#7d7d7d]"
+        />
+      </div>
+      <div className="grid sm:grid-cols-4 sm:grid-rows-2 gap-6 w-[80%]">
+        <div className="aspect-[3/4] bg-white border-b-2 border-[#7d7d7d] duration-200 hover:shadow-lg">
+          <div className="bg-gray-400 aspect-square"></div>
+          <div className="px-1">
+            <h1 className="font-bold">Shop Name</h1>
+            <p className="text-sm">Shop Description</p>
+          </div>
+        </div>
+        <div className="aspect-[3/4] bg-white border-b-2 border-[#7d7d7d] duration-200 hover:shadow-lg">
+          <div className="bg-gray-400 aspect-square"></div>
+          <div className="px-1">
+            <h1 className="font-bold">Shop Name</h1>
+            <p className="text-sm">Shop Description</p>
+          </div>
+        </div>
+        <div className="aspect-[3/4] bg-white border-b-2 border-[#7d7d7d] duration-200 hover:shadow-lg">
+          <div className="bg-gray-400 aspect-square"></div>
+          <div className="px-1">
+            <h1 className="font-bold">Shop Name</h1>
+            <p className="text-sm">Shop Description</p>
+          </div>
+        </div>
+        <div className="aspect-[3/4] bg-white border-b-2 border-[#7d7d7d] duration-200 hover:shadow-lg">
+          <div className="bg-gray-400 aspect-square"></div>
+          <div className="px-1">
+            <h1 className="font-bold">Shop Name</h1>
+            <p className="text-sm">Shop Description</p>
+          </div>
+        </div>
+        <div className="aspect-[3/4] bg-white border-b-2 border-[#7d7d7d] duration-200 hover:shadow-lg">
+          <div className="bg-gray-400 aspect-square"></div>
+          <div className="px-1">
+            <h1 className="font-bold">Shop Name</h1>
+            <p className="text-sm">Shop Description</p>
+          </div>
+        </div>
+        <div className="aspect-[3/4] bg-white border-b-2 border-[#7d7d7d] duration-200 hover:shadow-lg">
+          <div className="bg-gray-400 aspect-square"></div>
+          <div className="px-1">
+            <h1 className="font-bold">Shop Name</h1>
+            <p className="text-sm">Shop Description</p>
+          </div>
+        </div>
+        <div className="aspect-[3/4] bg-white border-b-2 border-[#7d7d7d] duration-200 hover:shadow-lg">
+          <div className="bg-gray-400 aspect-square"></div>
+          <div className="px-1">
+            <h1 className="font-bold">Shop Name</h1>
+            <p className="text-sm">Shop Description</p>
+          </div>
+        </div>
+        <div className="aspect-[3/4] bg-white border-b-2 border-[#7d7d7d] duration-200 hover:shadow-lg">
+          <div className="bg-gray-400 aspect-square"></div>
+          <div className="px-1">
+            <h1 className="font-bold">Shop Name</h1>
+            <p className="text-sm">Shop Description</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
