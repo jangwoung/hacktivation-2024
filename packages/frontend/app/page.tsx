b8763b23d9dbffc1d3ac53cc44993b5c36c9612d
jangwoung <masao31536@gmail.com>
2025-01-26 21:57:27 +0900

    [add] first setup

---
+++ packages/frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+export default function Home() {
+  return (
+    <div className="flex w-full justify-center items-center p-20">
+      <ConnectButton />
+    </div>
+  );
+}
---
+++ packages/frontend/wagmi.ts
@@ -0,0 +1,8 @@
+import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+import { sepolia, hardhat } from "wagmi/chains";
+
+export const config = getDefaultConfig({
+  appName: "RainbowKit App",
+  projectId: "YOUR_PROJECT_ID",
+  chains: [sepolia, hardhat],
+});
